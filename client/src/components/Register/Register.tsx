ac241f4c60ce37eb69ba373b9999ce8a512d2894
Ana Kreppel <anakreppel@gmail.com>
2022-01-29 13:26:48 +0100

    formik-forms-login-register

--- client/src/components/Register/Register.tsx
+++ client/src/components/Register/Register.tsx
@@ -1,45 +1,58 @@
 import React from 'react';
-import { Formik, Form } from 'formik';
-import { FormControl, FormLabel } from '@chakra-ui/react';
+import { Formik, Field, Form, FormikHelpers } from 'formik';
 
+interface Values {
+  username: string;
+  email: string;
+  address: string;
+  zipCode: string;
+  password: string;
+
+}
 export default function Register() {
   return (
-    <Formik
-      initialValues={{ username: '', password: '', email: '', address: '', zipcode: '' }}
-      onSubmit={(values) => { console.log(values) }}
-    >
-      {(values, handleChange) => (
+    <div>
+      <h1>Register</h1>
+      <Formik
+        initialValues={{
+          username: '',
+          address: '',
+          zipCode: '',
+          email: '',
+          password: '',
+        }}
+        onSubmit={(
+          values: Values,
+
+        ) => {
+          console.log('values', values);
+
+        }}
+      >
         <Form>
-          <h1>Register</h1>
-          <FormControl>
-            <FormLabel htmlFor='r-username'>Username</FormLabel>
-            <input className='username'
-              value={values.username} onChange={handleChange}
-              id='r-username' placeholder='username'
-            />
-            <FormLabel htmlFor='r-password'>Password</FormLabel>
-            <input className='register-password'
-              value={values.password} onChange={handleChange}
-              id='r-password' placeholder='password'
-            />
-            <FormLabel htmlFor='r-emal'>Email</FormLabel>
-            <input className='register-email'
-              value={values.email} onChange={handleChange}
-              id='r-email' placeholder='email'
-            />
-            <FormLabel htmlFor='r-address'>Adress</FormLabel>
-            <input className='address'
-              value={values.address} onChange={handleChange}
-              id='r-address' placeholder='address'
-            />
-            <FormLabel htmlFor='r-zipcode'>Zipcode</FormLabel>
-            <input className='zipcode'
-              value={values.zipcode} onChange={handleChange}
-              id='r-zipcode' placeholder='zipcode'
-            />
-          </FormControl>
+          <label htmlFor="username">Username</label>
+          <Field id="username" name="username" placeholder="username" />
+
+          <label htmlFor="email">Email</label>
+          <Field
+            id="email"
+            name="email"
+            placeholder="john@acme.com"
+            type="email"
+          />
+
+          <label htmlFor="address">Address</label>
+          <Field id="address" name="address" placeholder="Address" />
+
+          <label htmlFor="zipcode">ZIP Code</label>
+          <Field id="zipcode" name="zipCode" placeholder="ZIP Code" />
+
+          <label htmlFor="password">First Name</label>
+          <Field id="password" name="password" placeholder="Password" type='password' />
+
+          <button type="submit">Submit</button>
         </Form>
-      )}
-    </Formik >
+      </Formik>
+    </div>
   );
 }
