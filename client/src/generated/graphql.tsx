1d434a74dcffb4884633f7a51cf0e896ca6cb1c5
Felipe Braga <braga.felipe.edu@gmail.com>
2022-01-29 12:35:46 +0100

    Co-authored-by: Ana Caroline da Silva Kreppel <anakreppel@gmail.com> Co-authored-by: Andy Phung <andytphung@g.ucla.edu> Co-authored-by: Valeria Kondova <valeriakondova1@gmail.com>

--- client/src/generated/graphql.tsx
+++ client/src/generated/graphql.tsx
@@ -2,9 +2,15 @@ import gql from 'graphql-tag';
 import * as Urql from 'urql';
 export type Maybe<T> = T | null;
 export type InputMaybe<T> = Maybe<T>;
-export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
-export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
-export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+export type Exact<T extends { [key: string]: unknown }> = {
+  [K in keyof T]: T[K];
+};
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & {
+  [SubKey in K]?: Maybe<T[SubKey]>;
+};
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & {
+  [SubKey in K]: Maybe<T[SubKey]>;
+};
 export type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
 /** All built-in and custom scalars, mapped to their actual values */
 export type Scalars = {
@@ -18,13 +24,13 @@ export type Scalars = {
 export enum Allergies {
   GlutenFree = 'glutenFree',
   LactoseFree = 'lactoseFree',
-  NutFree = 'nutFree'
+  NutFree = 'nutFree',
 }
 
 export enum Diets {
   Pescatarian = 'pescatarian',
   Vegan = 'vegan',
-  Vegetarian = 'vegetarian'
+  Vegetarian = 'vegetarian',
 }
 
 export type Item = {
@@ -70,11 +76,9 @@ export type Mutation = {
   updateItem: Item;
 };
 
-
 export type MutationCreateItemArgs = {
   options: ItemCreateInput;
 };
-
 
 export type MutationUpdateItemArgs = {
   options: ItemUpdateInput;
@@ -85,7 +89,6 @@ export type Query = {
   getItem?: Maybe<Item>;
 };
 
-
 export type QueryGetItemArgs = {
   id: Scalars['Int'];
 };
@@ -94,87 +97,143 @@ export type Create_ItemMutationVariables = Exact<{
   options: ItemCreateInput;
 }>;
 
-
-export type Create_ItemMutation = { __typename?: 'Mutation', createItem: { __typename?: 'Item', id: number, name: string, description: string, servings: number, complete: boolean, archive: boolean, isGroceries: boolean, allergies: Array<Allergies>, diets: Array<Diets>, SICK_points: number, createdAt: string, updatedAt: string } };
+export type Create_ItemMutation = {
+  __typename?: 'Mutation';
+  createItem: {
+    __typename?: 'Item';
+    id: number;
+    name: string;
+    description: string;
+    servings: number;
+    complete: boolean;
+    archive: boolean;
+    isGroceries: boolean;
+    allergies: Array<Allergies>;
+    diets: Array<Diets>;
+    SICK_points: number;
+    createdAt: string;
+    updatedAt: string;
+  };
+};
 
 export type Update_ItemMutationVariables = Exact<{
   options: ItemUpdateInput;
 }>;
 
-
-export type Update_ItemMutation = { __typename?: 'Mutation', updateItem: { __typename?: 'Item', id: number, name: string, description: string, servings: number, complete: boolean, archive: boolean, isGroceries: boolean, allergies: Array<Allergies>, diets: Array<Diets>, SICK_points: number, createdAt: string, updatedAt: string } };
+export type Update_ItemMutation = {
+  __typename?: 'Mutation';
+  updateItem: {
+    __typename?: 'Item';
+    id: number;
+    name: string;
+    description: string;
+    servings: number;
+    complete: boolean;
+    archive: boolean;
+    isGroceries: boolean;
+    allergies: Array<Allergies>;
+    diets: Array<Diets>;
+    SICK_points: number;
+    createdAt: string;
+    updatedAt: string;
+  };
+};
 
 export type Get_ItemQueryVariables = Exact<{
   getItemId: Scalars['Int'];
 }>;
 
-
-export type Get_ItemQuery = { __typename?: 'Query', getItem?: { __typename?: 'Item', id: number, name: string, description: string, servings: number, complete: boolean, archive: boolean, isGroceries: boolean, allergies: Array<Allergies>, diets: Array<Diets>, SICK_points: number, createdAt: string, updatedAt: string } | null | undefined };
-
+export type Get_ItemQuery = {
+  __typename?: 'Query';
+  getItem?:
+    | {
+        __typename?: 'Item';
+        id: number;
+        name: string;
+        description: string;
+        servings: number;
+        complete: boolean;
+        archive: boolean;
+        isGroceries: boolean;
+        allergies: Array<Allergies>;
+        diets: Array<Diets>;
+        SICK_points: number;
+        createdAt: string;
+        updatedAt: string;
+      }
+    | null
+    | undefined;
+};
 
 export const Create_ItemDocument = gql`
-    mutation CREATE_ITEM($options: ItemCreateInput!) {
-  createItem(options: $options) {
-    id
-    name
-    description
-    servings
-    complete
-    archive
-    isGroceries
-    allergies
-    diets
-    SICK_points
-    createdAt
-    updatedAt
+  mutation CREATE_ITEM($options: ItemCreateInput!) {
+    createItem(options: $options) {
+      id
+      name
+      description
+      servings
+      complete
+      archive
+      isGroceries
+      allergies
+      diets
+      SICK_points
+      createdAt
+      updatedAt
+    }
   }
-}
-    `;
+`;
 
 export function useCreate_ItemMutation() {
-  return Urql.useMutation<Create_ItemMutation, Create_ItemMutationVariables>(Create_ItemDocument);
-};
-export const Update_ItemDocument = gql`
-    mutation UPDATE_ITEM($options: ItemUpdateInput!) {
-  updateItem(options: $options) {
-    id
-    name
-    description
-    servings
-    complete
-    archive
-    isGroceries
-    allergies
-    diets
-    SICK_points
-    createdAt
-    updatedAt
-  }
+  return Urql.useMutation<Create_ItemMutation, Create_ItemMutationVariables>(
+    Create_ItemDocument
+  );
 }
-    `;
+export const Update_ItemDocument = gql`
+  mutation UPDATE_ITEM($options: ItemUpdateInput!) {
+    updateItem(options: $options) {
+      id
+      name
+      description
+      servings
+      complete
+      archive
+      isGroceries
+      allergies
+      diets
+      SICK_points
+      createdAt
+      updatedAt
+    }
+  }
+`;
 
 export function useUpdate_ItemMutation() {
-  return Urql.useMutation<Update_ItemMutation, Update_ItemMutationVariables>(Update_ItemDocument);
-};
-export const Get_ItemDocument = gql`
-    query GET_ITEM($getItemId: Int!) {
-  getItem(id: $getItemId) {
-    id
-    name
-    description
-    servings
-    complete
-    archive
-    isGroceries
-    allergies
-    diets
-    SICK_points
-    createdAt
-    updatedAt
-  }
+  return Urql.useMutation<Update_ItemMutation, Update_ItemMutationVariables>(
+    Update_ItemDocument
+  );
 }
-    `;
+export const Get_ItemDocument = gql`
+  query GET_ITEM($getItemId: Int!) {
+    getItem(id: $getItemId) {
+      id
+      name
+      description
+      servings
+      complete
+      archive
+      isGroceries
+      allergies
+      diets
+      SICK_points
+      createdAt
+      updatedAt
+    }
+  }
+`;
 
-export function useGet_ItemQuery(options: Omit<Urql.UseQueryArgs<Get_ItemQueryVariables>, 'query'> = {}) {
+export function useGet_ItemQuery(
+  options: Omit<Urql.UseQueryArgs<Get_ItemQueryVariables>, 'query'> = {}
+) {
   return Urql.useQuery<Get_ItemQuery>({ query: Get_ItemDocument, ...options });
-};
+}
--- client/src/redux/reducers/rootReducer.ts
+++ client/src/redux/reducers/rootReducer.ts
@@ -1,5 +1,5 @@
 import { combineReducers } from 'redux';
-import { items } from './item';
+import { items } from './items';
 import { user } from './user';
 
 export const rootReducer = combineReducers({
