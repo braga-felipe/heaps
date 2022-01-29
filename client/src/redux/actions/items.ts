935fcb8e211319ce9d92013e090a2958eb96cebd
Felipe Braga <braga.felipe.edu@gmail.com>
2022-01-29 16:15:10 +0100

    redux set up done, need to connect with database

--- client/src/redux/actions/items.ts
+++ client/src/redux/actions/items.ts
@@ -1,11 +1,25 @@
-import { useGet_ItemQuery } from '../../generated/graphql';
-const [, /*{ fetching, data, error }*/ getItem] = useGet_ItemQuery();
+import {
+  useGet_ItemQuery,
+  useCreate_ItemMutation,
+} from '../../generated/graphql';
+
+// const [, /*{ fetching, data, error }*/ getItem] = useGet_ItemQuery();
+// const [, createItem] = useCreate_ItemMutation();
+
 export const getOneItem = () => {
   return async (dispatch, getState) => {
-    const item = await getItem();
+    // const item = await getItem();
+    const item = 'item is found!!';
+    console.log('item in reducer/action: ' + item);
     dispatch({
       type: 'GET_ITEM',
       payload: item,
     });
   };
 };
+
+// export const createOneItem = () => {
+//   return async (dispatch, getState) => {
+//     const item = await createItem();
+//   };
+// };
--- client/src/redux/reducers/items.ts
+++ client/src/redux/reducers/items.ts
@@ -1,7 +1,9 @@
 export const items = (state = [], action) => {
   switch (action.type) {
     case 'GET_ITEM': {
-      return [...state, action.payload];
+      console.log('resolver triggered GET_ITEM');
+      console.log('action.payload: ' + action.payload);
+      return action.payload;
     }
     default:
       return state;
