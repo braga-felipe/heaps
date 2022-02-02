6cdb6ed08ec8420192d87a56a163bc1e0a395587
Ana Kreppel <anakreppel@gmail.com>
2022-02-02 21:33:35 +0100

    home in progress

---
+++ client/src/components/Home/Home.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { useGet_All_ItemsQuery } from '../../generated/graphql';
+import Groceries from './Groceries';
+import Dishes from './Dishes';
+import { Button } from '@chakra-ui/react'
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react'
+import { useRouter } from 'next/router';
+import createItem from '../../pages/createItem';
+
+export default function Home() {
+  const [{ data }] = useGet_All_ItemsQuery();
+  const router = useRouter();
+
+  function showCreateItem() {
+    router.push('/' + 'createItem');
+  }
+
+  return (
+    <div>
+      <Tabs isFitted variant='enclosed'>
+        <TabList mb='1em'>
+          <Tab>Groceries</Tab>
+          <Tab>Dishes</Tab>
+        </TabList>
+        <TabPanels>
+          <TabPanel>
+            <Groceries />
+          </TabPanel>
+          <TabPanel>
+            <Dishes />
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
+      <Button onClick={showCreateItem}
+        size='md'
+        height='48px'
+        width='200px'
+        border='2px'
+        borderColor='green.500'
+      >
+        Post dish
+      </Button>
+    </div>
+  );
+}
--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,26 +1,15 @@
 import type { NextPage } from 'next';
 import styles from '../styles/Home.module.css';
 import { Container, Heading } from '@chakra-ui/react';
-import { useSelector, useDispatch } from 'react-redux';
-import { getOneItem } from '../redux/actions/items';
-
-import { useMeQuery } from '../generated/graphql';
+import Home from '../components/Home/Home';
 
 const IndexPage: NextPage = () => {
-  const dispatch = useDispatch();
-  dispatch(getOneItem());
-  const items = useSelector((state) => state.items);
 
-  const [{ data, fetching }] = useMeQuery();
 
   return (
     <Container className={styles.container}>
-      {data?.me ? (
-        <Heading size='lg'> Hello {data.me.username}</Heading>
-      ) : (
-        <Heading size='lg'>hello World!</Heading>
-      )}
-      <Heading size='md'>{items}</Heading>
+      <Heading>Home</Heading>
+      <Home />
     </Container>
   );
 };
