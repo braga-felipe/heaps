759c06fb7ac6411870c77a22477eaf31c7425210
Ana Kreppel <anakreppel@gmail.com>
2022-02-02 15:22:54 +0100

    router in login page working

--- client/src/components/Login/Login.tsx
+++ client/src/components/Login/Login.tsx
@@ -4,6 +4,7 @@ import InputField from '../ChakraUiComponents/InputField';
 import SubmitButton from '../ChakraUiComponents/Button';
 
 import { useUser_LoginMutation } from '../../generated/graphql';
+import { useRouter } from 'next/router';
 
 interface Values {
   email: string;
@@ -11,6 +12,7 @@ interface Values {
 }
 export default function Login() {
   const [, getUser] = useUser_LoginMutation();
+  const router = useRouter();
 
   return (
     <Formik
@@ -22,6 +24,9 @@ export default function Login() {
         const res = await getUser({ options: values })
         if (res.data?.userLogin.errors) {
           setErrors({ email: `${res.data.userLogin.errors[0].message}`, })
+        }
+        if (res.data?.userLogin.user) {
+          router.push("/");
         }
         console.log('values', values);
         console.log('login: ', res);
