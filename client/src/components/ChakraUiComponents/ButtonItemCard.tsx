a621dada2031c77b2b11a4d8da8bed7558e56f16
Valeria Kondova <valeriakondova1@gmail.com>
2022-02-07 18:13:32 +0100

    feat: create chat from home item card

--- client/src/components/ChakraUiComponents/ButtonItemCard.tsx
+++ client/src/components/ChakraUiComponents/ButtonItemCard.tsx
@@ -2,22 +2,37 @@ import { Button } from '@chakra-ui/react';
 import { useRouter } from 'next/router';
 import { useSelector } from 'react-redux';
 import { State } from '../../pages';
+import { useCreateChatMutation } from '../../generated/graphql';
+import checkIfChatExistsAndGenerateLink from '../../utils/checkIfChatExistsAndGenerateLink';
 
-export default function ItemCardButton({ buttonName, path }) {
+export default function ItemCardButton({ buttonName, path, item }) {
   const router = useRouter();
   const user = useSelector((state: State) => state.user);
-  function showPageLinked() {
+  const [, createNewChat] = useCreateChatMutation();
+
+  async function handleClick(item, userID) {
+    const url = checkIfChatExistsAndGenerateLink(item, userID);
+    if (url) {
+      router.push('/' + url);
+    } else {
+      const newChatID = await createNewChat({options: {userOwnerId: item.ownerId, userRequesterId: userID, itemOwnerId: item.id}});
+      router.push('/' + `chatLobby/?tid=${newChatID.data.createChat.id}&bool=true`);
+    }
+  }
+
+  function checkAuth(item, userID) {
     return Object.keys(user).length
-      ? router.push('/' + 'chatLobby/?tid=1&bool=false')
+      ? handleClick(item , userID)
       : router.push('/login');
   }
+
   return (
     <Button
       mt={4}
       size='xs'
       backgroundColor='secondaryActive'
       color='white'
-      onClick={showPageLinked}>
+      onClick={() => checkAuth(item, user.id)}>
       {buttonName}
     </Button>
   );
--- client/src/utils/checkIfChatExistsAndCreate.tsx
+++
@@ -1,9 +0,0 @@
-
-export default function checkIfChatExistsAndCreate(item, currentUserId) {
-  item.chats.forEach(chat => {
-    if (chat.users.forEach(user => user.id === currentUserId)) {
-      //string path to chat lobby 
-      return 
-    }
-  })
-}
