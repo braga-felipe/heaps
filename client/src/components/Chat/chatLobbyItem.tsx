e2580f976b6c7303119395fea4ace20ec23a15a6
Felipe Braga <braga.felipe.edu@gmail.com>
2022-02-07 18:08:31 +0100

    chatLobby styled

--- client/src/components/Chat/chatLobbyItem.tsx
+++ client/src/components/Chat/chatLobbyItem.tsx
@@ -1,37 +1,36 @@
 import React from 'react';
-import {
-  Box,
-  Container,
-  Flex,
-  Image,
-  Heading,
-} from '@chakra-ui/react';
+import { Box, Container, Flex, Image, Heading } from '@chakra-ui/react';
 
 interface chatLobbyItemProps {
-    itemName: string,
-    userName: string,
-    img_url: string,
-    lastMessageTime: string,
-    chatId: number
+  itemName: string;
+  userName: string;
+  img_url: string;
+  lastMessageTime: string;
+  chatId: number;
 }
 
-export const ChatLobbyItem: React.FC<chatLobbyItemProps> = ({itemName, img_url}) => {
-    
-    return (
+export const ChatLobbyItem: React.FC<chatLobbyItemProps> = ({
+  itemName,
+  img_url,
+}) => {
+  return (
     <Container sx={cStyle()}>
       <Flex sx={fStyle()}>
         <Heading sx={hStyle()}>{itemName}</Heading>
         <Box>
           <Image
             sx={iStyle()}
-            src={img_url || 'https://ca.slack-edge.com/T0WU5R8NT-U02GWRVJERW-72846fc663f1-512'}
+            src={
+              img_url ||
+              'https://ca.slack-edge.com/T0WU5R8NT-U02GWRVJERW-72846fc663f1-512'
+            }
             alt='profile'
           />
         </Box>
       </Flex>
     </Container>
   );
-}
+};
 
 function iStyle() {
   return {
@@ -49,10 +48,11 @@ function fStyle() {
 
 function cStyle() {
   return {
-    border: '1px solid',
-    marginTop: '3px',
-    width: '317px',
-    background: '#5D55B4',
+    border: '1px solid #E2E8F0',
+    marginTop: '60px',
+    marginLeft: '-10px',
+    width: '325px',
+    background: 'primary',
     borderRadius: ' 10px',
     boxShadow: '3px 3px 10px rgba(116, 65, 0, 0.2)',
   };
@@ -61,12 +61,10 @@ function cStyle() {
 function hStyle() {
   return {
     fontStyle: 'normal',
-    fontWeight: '5000',
+    fontWeight: '500',
     fontSize: '20px',
     lineHeight: '22px',
   };
 }
 
 export default ChatLobbyItem;
-
-
