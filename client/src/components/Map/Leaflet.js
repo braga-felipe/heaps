a0eb10668bf28692e4c434a63b42ee4b27dbf798
Felipe Braga <braga.felipe.edu@gmail.com>
2022-02-08 17:12:36 +0100

    merge conflict

--- client/src/components/Map/Leaflet.js
+++ client/src/components/Map/Leaflet.js
@@ -7,7 +7,6 @@ const Map = ({ position }) => {
     iconSize: [38, 95],
     iconAnchor: [22, 94],
     popupAnchor: [-3, -76],
-    shadowUrl: 'my-icon-shadow.png',
     shadowSize: [68, 95],
     shadowAnchor: [22, 94],
   });
