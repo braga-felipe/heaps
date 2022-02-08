76f2ad2003c88714a4788153ff07bd25762bd199
Felipe Braga <braga.felipe.edu@gmail.com>
2022-02-08 19:49:23 +0100

    marker in leaflet

--- client/src/components/Map/Leaflet.js
+++ client/src/components/Map/Leaflet.js
@@ -1,14 +1,12 @@
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import Loading from '../Assets/Loading';
+
 const Map = ({ position }) => {
-  const icon = L.icon({
-    iconUrl: 'purple_pin_full.png',
-    iconSize: [38, 95],
-    iconAnchor: [22, 94],
-    popupAnchor: [-3, -76],
-    shadowSize: [68, 95],
-    shadowAnchor: [22, 94],
+  const userIcon = L.icon({
+    iconUrl:
+      'https://cdn-icons.flaticon.com/png/512/2776/premium/2776067.png?token=exp=1644346881~hmac=da83c5327c469f4e4bba422270f70646',
+    iconSize: [38, 38],
   });
 
   return position.longitude ? (
@@ -21,10 +19,10 @@ const Map = ({ position }) => {
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
       />
-      <Marker icon={icon} position={[position.latitude, position.longitude]}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
+      <Marker
+        icon={userIcon}
+        position={[position.latitude, position.longitude]}>
+        <Popup>You're here.</Popup>
       </Marker>
     </MapContainer>
   ) : (
