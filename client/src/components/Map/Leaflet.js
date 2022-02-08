698132d7edb6ee8d20830413496114d9371c8ab8
Felipe Braga <braga.felipe.edu@gmail.com>
2022-02-08 16:47:48 +0100

    map responsive to location

--- client/src/components/Map/Leaflet.js
+++ client/src/components/Map/Leaflet.js
@@ -1,10 +1,20 @@
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
+import Loading from '../Assets/Loading';
+const Map = ({ position }) => {
+  const icon = L.icon({
+    iconUrl: 'purple_pin_full.png',
+    iconSize: [38, 95],
+    iconAnchor: [22, 94],
+    popupAnchor: [-3, -76],
+    shadowUrl: 'my-icon-shadow.png',
+    shadowSize: [68, 95],
+    shadowAnchor: [22, 94],
+  });
 
-const Map = () => {
-  return (
+  return position.longitude ? (
     <MapContainer
-      center={[52.49466617163345, 13.411438757658564]}
+      center={[position.latitude, position.longitude]}
       zoom={13}
       scrollWheelZoom={false}
       style={{ height: 400, width: '100%' }}>
@@ -12,12 +22,14 @@ const Map = () => {
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
       />
-      <Marker position={[52.49466617163345, 13.411438757658564]}>
+      <Marker icon={icon} position={[position.latitude, position.longitude]}>
         <Popup>
           A pretty CSS3 popup. <br /> Easily customizable.
         </Popup>
       </Marker>
     </MapContainer>
+  ) : (
+    <Loading />
   );
 };
 
