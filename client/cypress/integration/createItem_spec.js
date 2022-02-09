a710ddabe41482922e4d019790f9360c43853c4d
Ana Kreppel <anakreppel@gmail.com>
2022-02-09 11:55:44 +0100

    createItem test working

--- client/cypress/integration/createItem_spec.js
+++ client/cypress/integration/createItem_spec.js
@@ -1,14 +1,28 @@
 describe("Create a new food Item", function () {
-  beforeEach(() => {
+  before(() => {
     cy.visit('http://localhost:3000/login')
     cy.login('anatest4@test4.com', '12345678')
+    cy.wait(2000)
+    cy.get('#createItemIcon').click()
+    cy.wait(2000)
   });
+
   it('Cretate item without any inputs', () => {
-    cy.visit('http://localhost:3000/createItem')
     cy.contains('Create a Food Item').click()
     cy.get('form').submit()
-
     cy.get('#field-1-feedback').should('be.visible');
     cy.get("#field-2-feedback").should('be.visible');
   });
+
+  it('Create item', () => {
+    cy.contains('Create a Food Item')
+
+    cy.get('input[name=name]')
+      .type('Fake Food')
+      .should('have.value', 'Fake Food')
+
+    cy.get('input[name=description]')
+      .type('Description')
+      .should('have.value', 'Description')
+  })
 });
--- client/cypress/support/commands.js
+++ client/cypress/support/commands.js
@@ -27,6 +27,6 @@
 Cypress.Commands.add('login', (email, password) => {
   cy.get('#email').type(email)
   cy.get('#password').type(password)
-  cy.contains('Log In').click()
-  cy.url().should('contain', '/')
+  cy.get('button').click()
+  cy.wait(2000)
 })
